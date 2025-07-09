0e34b0cd297a9bcd16715158aad9a69232482f22
AntonioAcevedo1178 <20243ds076@utez.edu.mx>
2025-07-08 22:07:42 -0600

    Tipografía cambiada en todas las vistas (creo) xD

--- src/main/java/com/utez/calendario/MainApp.java
+++ src/main/java/com/utez/calendario/MainApp.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -12,6 +13,9 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        loadInriaSansFonts();
+
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
 
         Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
@@ -27,6 +31,23 @@ public class MainApp extends Application {
         primaryStage.setMinHeight(600);
         primaryStage.show();
         primaryStage.centerOnScreen();
+    }
+
+    private void loadInriaSansFonts() {
+        try {
+            Font.loadFont(getClass().getResourceAsStream("/fonts/InriaSans-Regular.ttf"), 12);
+            Font.loadFont(getClass().getResourceAsStream("/fonts/InriaSans-Bold.ttf"), 12);
+            Font.loadFont(getClass().getResourceAsStream("/fonts/InriaSans-Italic.ttf"), 12);
+            Font.loadFont(getClass().getResourceAsStream("/fonts/InriaSans-BoldItalic.ttf"), 12);
+            Font.loadFont(getClass().getResourceAsStream("/fonts/InriaSans-Light.ttf"), 12);
+            Font.loadFont(getClass().getResourceAsStream("/fonts/InriaSans-LightItalic.ttf"), 12);
+            Font.loadFont(getClass().getResourceAsStream("/fonts/InriaSans-SemiBold.ttf"), 12);
+
+            System.out.println("Fuentes Inria Sans cargadas exitosamente");
+        } catch (Exception e) {
+            System.err.println("Error al cargar las fuentes Inria Sans: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
